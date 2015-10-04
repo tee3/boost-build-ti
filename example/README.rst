cf5fae24298885e3a3fd9965aaabd7e1d8b7c22f
Thomas Brown <tabsoftwareconsulting@gmail.com>
2015-10-04 12:58:49 -0400

    example: Added a script to iterate over build parameters.
    This is the beginning of a test script.  This also should support
    additional toolsets, architectures, and instruction sets for future
    additions.

--- example/README.rst
+++ example/README.rst
@@ -12,11 +12,23 @@ functionality of the TMS320 toolsets.
 Usage
 -----
 
-To run this example, run the following command.
+To run this example for the TMS320C6000 TMS320C64x+ processing
+defaulting the rest of the build parameters, run the following
+command.
 
 ::
 
    $ b2 toolset=tms320c6000 architecture=tms320c6000 instruction-set=tms320c64x+ link=static
+
+To run all the supported toolsets, architectures, and instruction sets
+against the main build parameters, run the following command.  This
+will attempt to build a very simple project varying the variant
+(debug, release), linking (static, shared), exception handling (off,
+on), and RTTI (off, on).
+
+::
+
+   $ ./build.sh
 
 Note that there is not currently any way to run the resulting programs
 on the host system since Texas Instruments does not generally provide
---
+++ example/build.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+b2="bjam"
+
+iterate_toolset_instruction_sets () {
+    toolset=${1}
+    architecture=${2}
+    instruction_sets=${3}
+
+    for instruction_set in ${instruction_sets} ; do
+        for variant in debug release ; do
+            for link in static shared ; do
+                for exception_handling in off on ; do
+                    for rtti in off on ; do
+                        ${b2} \
+                            toolset="${toolset}" \
+                            architecture="${architecture}" \
+                            instruction-set="${instruction_set}" \
+                            variant="${variant}" \
+                            link="${link}" \
+                            exception-handling="${exception_handling}" \
+                            rtti="${rtti}"
+                    done
+                done
+            done
+        done
+    done
+}
+
+iterate_toolset_instruction_sets tms320c6000-7.4.14 tms320c6000 tms320c6000 tms320c6200 tms320c6400
